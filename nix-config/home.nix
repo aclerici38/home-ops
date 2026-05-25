c8db738eed82beaf75b97c0b96ce2fefa39a578e
aclerici38 <anthony@clerici.me>
2026-05-25 16:19:41 -0700

    fix: bad config

--- nix-config/home.nix
+++ nix-config/home.nix
@@ -60,9 +60,7 @@
 
   programs.ssh = {
     enable = true;
-    settings = {
-      IdentityAgent = "\"~/Library/Group Containers/2BUA8C4S2C.com.1password/t/agent.sock\"";
-    };
+    settings."*".IdentityAgent = "\"~/Library/Group Containers/2BUA8C4S2C.com.1password/t/agent.sock\"";
   };
 
   programs.git = {
