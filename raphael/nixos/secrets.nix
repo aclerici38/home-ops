09b95606641f3b1e87f80d21841224557a58a140
aclerici38 <anthony@clerici.me>
2026-07-06 14:59:03 -0700

    chore: new sops setup

--- raphael/nixos/secrets.nix
+++ raphael/nixos/secrets.nix
@@ -1,12 +1,7 @@
 { config, ... }:
-# sops-nix: age key = the host's SSH key (generated at install), so there's no
-# separate key to place. Bootstrap note in README: secrets.sops.yaml is currently
-# encrypted to the repo age key; after first boot, add the host key as a recipient
-# (ssh-to-age /etc/ssh/ssh_host_ed25519_key.pub) and `sops updatekeys`.
 {
   sops.defaultSopsFile = ../secrets.sops.yaml;
   sops.age.sshKeyPaths = [ "/etc/ssh/ssh_host_ed25519_key" ];
-  sops.age.keyFile = "/var/lib/sops-nix/keys.txt"; # bootstrap: repo age key lives here
 
   sops.secrets = {
     cloudflare-token = { };
