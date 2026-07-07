ff21e406d7acc0328b7db3a57465759b7059fd14
aclerici38 <anthony@clerici.me>
2026-07-07 09:10:04 -0700

    more config

--- raphael/nixos/k3s.nix
+++ raphael/nixos/k3s.nix
@@ -19,7 +19,31 @@ in
     enable = true;
     role = "server";
     package = k3sPkgs.${k3sSlot};
-    # Traefik is bundled by default; this box fronts everything with Caddy.
-    extraFlags = [ "--disable=traefik" ];
+
+    extraFlags = [
+      "--disable=traefik"
+      "--disable=coredns"
+      "--flannel-backend=host-gw"
+      "--disable-network-policy"
+      "--disable-helm-controller"
+      "--egress-selector-mode=disabled"
+      "--write-kubeconfig-mode=0600"
+
+      "--kubelet-arg=image-gc-high-threshold=75" # start pruning images at 75% imagefs
+      "--kubelet-arg=image-gc-low-threshold=60" # prune down to 60%
+      "--kubelet-arg=system-reserved=cpu=200m,memory=256Mi"
+      "--kubelet-arg=eviction-hard=memory.available<100Mi,nodefs.available<10%,imagefs.available<10%"
+
+      "--kubelet-arg=node-status-update-frequency=20s"
+      "--kube-controller-manager-arg=node-monitor-period=20s"
+      "--kube-controller-manager-arg=node-monitor-grace-period=1m"
+      "--kube-apiserver-arg=event-ttl=30m"
+    ];
+  };
+
+  # Firewall: allow the apiserver and never filter Cilium/pod interfaces.
+  networking.firewall = {
+    allowedTCPPorts = [ 6443 ];
+    trustedInterfaces = [ "cni0" ];
   };
 }
