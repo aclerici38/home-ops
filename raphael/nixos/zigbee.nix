0f414f4204be0e3d624a18f3c31a35a57a285a07
aclerici38 <anthony@clerici.me>
2026-07-12 16:12:10 -0700

    fix: forgot the udev rule

--- raphael/nixos/zigbee.nix
+++ raphael/nixos/zigbee.nix
@@ -1,7 +1,7 @@
 { ... }:
 {
-  # Sonoff Zigbee 3.0 USB Dongle Plus V2 (CC2652 / CP2102N) for zigbee2mqtt.
+  # Sonoff Zigbee Dongle Lite (EFR32MG21 / CP2102N) for zigbee2mqtt.
   services.udev.extraRules = ''
-    SUBSYSTEM=="tty", ATTRS{serial}=="d4f0088f4c1fef119b9450d0639e525b", GROUP="20", MODE="0660"
+    SUBSYSTEM=="tty", ATTRS{serial}=="046deb1ea4a3ef1187ab4abd61ce3355", GROUP="20", MODE="0660"
   '';
 }
