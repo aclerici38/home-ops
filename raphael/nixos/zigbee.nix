5c61f6c4c60a90c9ab8db4bd81ae4d37f8169e3f
aclerici38 <anthony@clerici.me>
2026-07-13 12:32:04 -0700

    fix: format

--- raphael/nixos/zigbee.nix
+++ raphael/nixos/zigbee.nix
@@ -1,5 +1,4 @@
-_:
-{
+_: {
   # Sonoff Zigbee Dongle Lite (EFR32MG21 / CP2102N) for zigbee2mqtt.
   services.udev.extraRules = ''
     SUBSYSTEM=="tty", ATTRS{serial}=="046deb1ea4a3ef1187ab4abd61ce3355", GROUP="20", MODE="0660"
